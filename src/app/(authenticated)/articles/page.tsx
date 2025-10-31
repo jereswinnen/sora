c61f695b0c711426a9a0d28c50a9af974c5692b3
Jeremy Swinnen <hey@jeremys.be>
2025-10-31 22:14:46 +0100

    Removed unused imports

--- src/app/(authenticated)/articles/page.tsx
+++ src/app/(authenticated)/articles/page.tsx
@@ -8,7 +8,6 @@ import { Id } from "../../../../convex/_generated/dataModel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Spinner } from "@/components/ui/spinner";
 import { cn } from "@/lib/utils";
 import { toast } from "sonner";
